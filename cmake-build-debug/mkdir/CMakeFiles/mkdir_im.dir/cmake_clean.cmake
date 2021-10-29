43d21cc5b41d0693848d81688c2f3f353215d613
alex <aangeletakis@gmail.com>
2021-10-28 20:12:11 -0400

    Created a C++ version of mkdir that's cross platform

---
+++ cmake-build-debug/mkdir/CMakeFiles/mkdir_im.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mkdir_im.dir/main.cpp.o"
+  "mkdir_im"
+  "mkdir_im.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mkdir_im.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
