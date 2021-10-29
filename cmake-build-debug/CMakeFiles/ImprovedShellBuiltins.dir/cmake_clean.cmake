553f45535b0071626cdebb07938a013a5075762e
alex <aangeletakis@gmail.com>
2021-10-28 20:12:11 -0400

    Created a C++ version of mkdir that's cross platform

---
+++ cmake-build-debug/CMakeFiles/ImprovedShellBuiltins.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImprovedShellBuiltins.dir/src/mkdir.cpp.o"
+  "ImprovedShellBuiltins"
+  "ImprovedShellBuiltins.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImprovedShellBuiltins.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/hello_headers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_headers.dir/src/mkdir.cpp.o"
+  "hello_headers"
+  "hello_headers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_headers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
